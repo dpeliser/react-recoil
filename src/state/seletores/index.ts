f379877448e03ff3253183d33fc737b0124063fc
Diego Peliser <peliser.diego@gmail.com>
2022-06-29 16:27:58 -0300

    Event Tracker

---
+++ src/state/seletores/index.ts
@@ -0,0 +1,37 @@
+import { selector } from 'recoil';
+import { IEvento } from '../../interfaces/IEvento';
+import { filtroDeEventos, listaDeEventosState } from '../atom';
+
+export const eventosFiltradosState = selector({
+  key: "eventosFiltradosState",
+  get: ({ get }) => {
+    const filtro = get(filtroDeEventos);
+    const todosEventos = get(listaDeEventosState);
+
+    const eventos = todosEventos.filter(evento => {
+      if (!filtro.data) {
+        return true;
+      }
+  
+      const mesmoDia = filtro.data.toISOString().slice(0, 10)
+        === evento.inicio.toISOString().slice(0, 10);
+      return mesmoDia;
+    });
+
+    return eventos;
+  }
+});
+
+export const eventosAsync = selector({
+  key: "eventosAsync",
+  get: async () => {
+    const resposta = await fetch('http://localhost:8080/eventos');
+    const eventos: IEvento[] = await resposta.json();
+
+    return eventos.map(evento => ({
+      ...evento,
+      inicio: new Date(evento.inicio),
+      fim: new Date(evento.fim),
+    }));
+  },
+});
